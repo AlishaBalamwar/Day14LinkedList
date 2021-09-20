d7259ba110548752e0c197f29f9c3253f73db81a
AlishaBalamwar <alishabalamwar7806@gmail.com>
2021-09-20 16:08:09 +0530

    Added nodes to the end of list

--- src/com/linkedlist/LinkedList1.java
+++ src/com/linkedlist/LinkedList1.java
@@ -25,18 +25,31 @@ public class LinkedList1 {
 		System.out.println("null");
 	}
 	
-	public void insertAtBegining(int value) {
+	/*public void insertAtBegining(int value) {
 		ListNode newNode = new ListNode(value);
 		newNode.next = head;
 		head = newNode;
+	}*/
+	
+	public void insertAtEnd(int value) {
+		ListNode newNode = new ListNode(value);
+		if(head == null) {
+			head = newNode;
+			return;
+		}
+		ListNode current =head;
+		while(current.next!= null) {
+			current = current.next;
+		}
+		current.next = newNode;
 	}
 	
 	public static void main(String[] args) {
 		LinkedList1 linkedList1 = new LinkedList1();
 		
-		linkedList1.insertAtBegining(70);
-		linkedList1.insertAtBegining(30);
-		linkedList1.insertAtBegining(56);
+		linkedList1.insertAtEnd(56);
+		linkedList1.insertAtEnd(30);
+		linkedList1.insertAtEnd(70);
 		linkedList1.displayLinkedList();
 	}
 }
